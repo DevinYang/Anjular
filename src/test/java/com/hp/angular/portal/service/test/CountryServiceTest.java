76a11e999bcbc9eaa33375c45d4ed63f79ce8aad
tojaoomy <tojaoomy@yahoo.cn>
2014-03-06 19:11:45 +0800

    add the feature profile & memory database hsqldb

--- src/test/java/com/hp/angular/portal/service/test/CountryServiceTest.java
+++ src/test/java/com/hp/angular/portal/service/test/CountryServiceTest.java
@@ -3,6 +3,7 @@ package com.hp.angular.portal.service.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,7 +12,8 @@ import static org.junit.Assert.*;
 import com.hp.angular.portal.service.CountryService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:application-context.xml")
+@ContextConfiguration(locations={"classpath:application-context.xml","classpath:profile.xml"})
+@ActiveProfiles("test")
 public class CountryServiceTest {
 	
 	@Autowired
